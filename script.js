287a11e91fecac1154cf6c38b071636540ad3474
ahvShiro <arthurshiro123@gmail.com>
2024-10-29 09:45:10 -0300

    Arquivos adicionados

---
+++ script.js
@@ -0,0 +1,35 @@
+let currentScreen = 1;
+
+
+
+function goToNextScreen() {
+    if (currentScreen === 2 && document.getElementById('objetivo').value === 'restaurantes') {
+        window.location.href = 'restaurantes.html';  // Redirect to restaurantes.html
+    } else {
+        document.getElementById(`screen${currentScreen}`).classList.remove('active');
+        currentScreen++;
+        if (document.getElementById(`screen${currentScreen}`)) {
+            document.getElementById(`screen${currentScreen}`).classList.add('active');
+        }
+    }
+}
+
+
+function goToPreviousScreen() {
+    if (currentScreen > 1) {
+        document.getElementById(`screen${currentScreen}`).classList.remove('active');
+        currentScreen--;
+        document.getElementById(`screen${currentScreen}`).classList.add('active');
+    }
+}
+
+
+function adjustMenu() {
+    alert('Função para ajustar o cardápio ainda em desenvolvimento!');
+}
+
+function finishApp() {
+    alert('Seu plano foi salvo!');
+    window.location.href = 'restaurantes.html';  // Redirect to restaurantes.html
+}
+
